37f2c5b14aefff974582a48c4413325cd9b27ad1
shubhamChauhan <shubhamchauhan4488@gmail.com>
2020-05-21 00:33:41 -0400

    Add axios, axios call and setup orchestration for businesses

---
+++ frontend/ui/src/modules/businesses/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+const getFetchBusinessesUrl = () => `http://127.0.0.1:8000/api/businesses/`;
+
+const businessesApi = {
+  tryFetchBusinesses: async () => {
+    try {
+      const url = getFetchBusinessesUrl();
+      const response = await axios.get(url);
+
+      return response?.data;
+    } catch (e) {
+        console.log('Error Details: ',e.toJson())
+      throw e;
+    }
+  },
+};
+
+export default businessesApi;
