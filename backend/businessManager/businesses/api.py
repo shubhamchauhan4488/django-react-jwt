19e7114587f4836a1fe36209e20b2b462154110e
shubhamChauhan <shubhamchauhan4488@gmail.com>
2020-07-17 00:02:58 -0400

    Operating hours viewest

--- backend/businessManager/businesses/api.py
+++ backend/businessManager/businesses/api.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from businesses.models import Business
-from .serializers import BusinessSerializer
+from businesses.models import Business, OperatingHours
+from .serializers import BusinessSerializer, OperatingHoursSerializer
 
 # Business viewset :
 # Helps to create a basic CRUD api without having to specify explicitly GET POST PUT etc
@@ -24,3 +24,18 @@ class BusinessViewSet(viewsets.ModelViewSet):
         # To inject additional data at the point of saving the instance. This additional data might include information like the current user, the current time,etc
         # You can do so by including additional keyword arguments when calling .save()
         serializer.save(owner=self.request.user)
+
+
+class OperatingHoursViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = OperatingHoursSerializer
+
+    def get_queryset(self):
+        print('selfkwargs', self.kwargs['id'])
+        queryset = OperatingHours.objects.filter(
+            business_id=self.kwargs['id'])
+        print('queryset', queryset)
+        return queryset
